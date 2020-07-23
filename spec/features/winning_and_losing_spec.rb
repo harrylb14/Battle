847487bf02d1985af0dac31c532feb3e97abc349
Harry LB <harry.lingardbright@gmail.com>
2020-07-23 18:20:55 +0100

    remove comments and add test

--- spec/features/winning_and_losing_spec.rb
+++ spec/features/winning_and_losing_spec.rb
@@ -1,8 +1,15 @@
 feature 'losing' do
-  scenario 'player 2 reaches 0HP before player 1' do
+  scenario 'player 1 reaches 0HP before player 2' do
     srand(67809)
     sign_in_and_play
     both_attack_multiple_times(10)
     expect(page).to have_content  "Haz is the loser!"
+  end
+  scenario 'player 2 reaches 0HP before player 1' do
+    srand(67810)
+    sign_in_and_play
+    both_attack_multiple_times(7)
+    attack_confirm
+    expect(page).to have_content  "Haz2 is the loser!"
   end
 end
